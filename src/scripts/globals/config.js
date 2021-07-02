7f06e6d685f55d8714dec54f785938a5691db0c1
Pradipta A. Suryadi <116080050.pas@gmail.com>
2021-07-02 22:19:41 +0700

    Add config.js back

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  KEY: '12345',
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_IMAGE_URL: (size) => `https://restaurant-api.dicoding.dev//images/${size}/`,
+  DEFAULT_LANGUAGE: 'en-us',
+  // CACHE_NAME: 'RestaurantApps-V1',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'restaurants-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurants',
+};
+
+export default CONFIG;
